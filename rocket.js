c619043682726b4f6bcbaf83ff0cbdfe8681ce1e
Ellie Krugler <elliekrugler@gmail.com>
2023-05-30 20:57:19 -0700

    cleaned up date selection

--- rocket.js
+++ rocket.js
@@ -102,16 +102,19 @@ export class Rocket extends Scene {
     }
 
     date_select(recipient = this, parent = this.control_panel) {
-        const date = parent.appendChild(document.createElement("INPUT"));
-        date.setAttribute("id", "date");
-        date.setAttribute("type", "date");
-        date.setAttribute("value", "2023-05-25");
+        const date_widget = parent.appendChild(document.createElement("INPUT"));
+        date_widget.setAttribute("id", "date");
+        date_widget.setAttribute("type", "date");
+        date_widget.setAttribute("value", this.date);
+
+        let current_date = new Date(this.date);
+        this.lastTGen = (getJulianDate_Planets(current_date.getFullYear(), current_date.getMonth(), current_date.getDay())-2451545.0)/36525;
 
         const new_date = () => {
-            this.date = date.value;
+            this.date = date_widget.value;
         }
 
-        date.addEventListener("change", new_date);
+        date_widget.addEventListener("change", new_date);
     }
 
 
@@ -125,7 +128,7 @@ export class Rocket extends Scene {
         this.paused = false;
         this.date_select();
         this.new_line();
-        this.key_triggered_button("Pause", ["Control", "0"], () => this.paused = () => !(this.paused));
+        this.key_triggered_button("Pause/Play", ["Control", "0"], () => this.pause());
         // this.key_triggered_button("View solar system", ["Control", "0"], () => this.attached = () => null);
         // this.new_line();
         // this.key_triggered_button("Attach to planet 1", ["Control", "1"], () => this.attached = () => this.planet_1);
@@ -135,6 +138,10 @@ export class Rocket extends Scene {
         // this.key_triggered_button("Attach to planet 4", ["Control", "4"], () => this.attached = () => this.planet_4);
         // this.new_line();
         // this.key_triggered_button("Attach to moon", ["Control", "m"], () => this.attached = () => this.moon);
+    }
+
+    pause() {
+        this.paused = !(this.paused);
     }
 
     display(context, program_state) {
@@ -218,12 +225,13 @@ export class Rocket extends Scene {
         let saturn_num = 5;
         let uranus_num = 6;
         let neptune_num = 7;
-        // let TGen = 0.0015*t;
 
         let current_date = new Date(this.date);
         let julianDate = getJulianDate_Planets(current_date.getFullYear(), current_date.getMonth(), current_date.getDay());
-        let TGen = this.paused ? (julianDate-2451545.0)/36525 : 0.0015*t;
-
+        let widget_TGen = (julianDate-2451545.0)/36525;
+        let continuous_TGen = this.lastTGen + 0.0015*dt;
+        let TGen = this.paused ? widget_TGen : continuous_TGen;
+        this.lastTGen = TGen;
 
 		
         let merc_coords = plotPlanet_Planets(TGen, 0);
